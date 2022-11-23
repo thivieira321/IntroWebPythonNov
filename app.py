b5713fdfd604226b25ccd71dcb5aada43e472546
thivieira321 <vieiranet@msn.com>
2022-11-23 20:21:45 +0000

    criação banco, alteração html

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+import sqlite3
 
-app = Flask("Olá")
+app = Flask("Olá Mundo")
+
+DATABASE = "banco.bd"
+SECRET_KEY = "1234"
+
+app = Flask("Olá Mundo")
+
+app.config.from_object(__name__)
+
+def conectar():
+    return sqlite3.connect(DATABASE)
+
+@app.before_request
+def before_request():
+    g.bd = conectar()
+
+@app.teardown_request
+def teardown_request(f):
+    g.bd.close()
 
 @app.route("/")
-def hello():
-    return "Olá Mundo!"
+def exibir_posts():
+    sql = "SELECT titulo, texto, data_criacao FROM posts ORDER BY id DESC"
+    resultado = g.bd.execute(sql)
 
-@app.route("/alunos")
-def alunos():
-    return render_template("hello.html")
+    post = {"titulo":"Meu Título", "texto":"Primeiro texto","data_criacao":"23/11/2022"}
+    return render_template("hello.html", post = post)
 
